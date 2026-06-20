6a63d8b997e6f4eac60761f39ca79c0cbb8ed9d7
Vinezin-hackerwer <viniapelaun306@gmail.com>
2026-06-20 16:36:14 -0300

    Update item_schema.py

--- app/schemas/item_schema.py
+++ app/schemas/item_schema.py
@@ -2,16 +2,16 @@ from pydantic import BaseModel
 from typing import Optional
 from datetime import datetime
 
-class TaskBase(BaseModel):
+class ItemBase(BaseModel):
     title: str
     description: Optional[str] = None
     priority: str
     status: str = "Pendente"
 
-class TaskCreate(TaskBase):
+class ItemCreate(ItemBase):
     pass
 
-class TaskResponse(TaskBase):
+class ItemResponse(ItemBase):
     id: int
     created_at: datetime
 
